d7c691be765f1cf5eae1892f2dd3f094ad8493ec
CandyKan3 <41598791+CandyKan3@users.noreply.github.com>
2019-04-25 21:33:36 -0400

    Added login functionality, will not run without the JSON file that connects, was removed for security purposes on upload.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() => runApp(MyApp());
 
@@ -50,19 +51,116 @@ class MyTextInput extends StatefulWidget{
   MyTextInputState createState() => new MyTextInputState();
 }
 
+enum FormType{
+  login,
+  register
+}
 class MyTextInputState extends State<MyTextInput>{
+  String _email;
+  String _password;
+  FormType _formType = FormType.login;
+  final formKey = new GlobalKey<FormState>();
+
+bool validateAndSave(){
+  final form = formKey.currentState;
+  if(form.validate()){
+    form.save();
+    return true;
+  }
+  else{
+    return false;
+    // print('Form is invalid');
+  }
+}
+void validateAndSubmit()async {
+  if(validateAndSave()){
+    try {
+      if(_formType ==FormType.login) {
+        FirebaseUser user = await FirebaseAuth.instance
+            .signInWithEmailAndPassword(email: _email, password: _password);
+        print('Signed in: ${user.uid}');
+      }
+      else{
+        FirebaseUser user = await FirebaseAuth.instance
+            .createUserWithEmailAndPassword(email: _email, password: _password);
+        print('Created user: ${user.uid}');
+      }
+    }
+    catch(e){
+      print('Error: $e');
+    }
+  }
+}
+void moveToRegister(){
+  setState(() {
+    _formType = FormType.register;
+  });
+
+}
+void moveToLogin(){
+  setState(() {
+    _formType = FormType.login;
+  });
+
+}
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
     appBar: new AppBar(title: new Text("UNCC Schedule"), backgroundColor: Colors.green),
         body: new Container(
-            child: new Center(
-                child: new Column(
-                  children: <Widget> [ new TextField( decoration: new InputDecoration(
-                    hintText: "Please enter the time of your first class!"
-                  ))]
-                ))));
+          padding: EdgeInsets.all(16.0),
+            child: new Form(
+              key: formKey,
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children:buildInputs() + buildSubmitButtons()
+              ),)
+//            new Center(
+//                child: new Column(
+//                  children: <Widget> [ new TextField( decoration: new InputDecoration(
+//                    hintText: "Please enter the time of your first class!"
+//                  ))]
+                ));
+  }
+  List<Widget> buildInputs(){
+  return[ new TextFormField(
+    decoration: new InputDecoration(labelText: 'Email'),
+    validator: (value) => value.isEmpty ? 'Email Can\'t be empty': null,
+    onSaved: (value) => _email = value,
+  ),
+  new TextFormField(
+    decoration: new InputDecoration(labelText: 'Password'),
+    obscureText: true,
+    validator: (value) => value.isEmpty ? 'Password Can\'t be empty': null,
+    onSaved: (value) => _password = value,
+  )
+  ];}
+  List<Widget> buildSubmitButtons(){
+  if(_formType == FormType.login){
+  return[
+
+    new RaisedButton(
+      child: new Text('Login', style: new TextStyle(fontSize: 20.0)),
+      onPressed: validateAndSubmit,
+    ),
+    new FlatButton(
+      child: new Text('Create an account', style: new TextStyle(fontSize:20.0)),
+      onPressed: moveToRegister,
+    )
+  ];}
+  else{
+    return[
+      new RaisedButton(
+        child: new Text('Create an account', style: new TextStyle(fontSize: 20.0)),
+        onPressed: validateAndSubmit,
+      ),
+      new FlatButton(
+        child: new Text('Have an account? login!', style: new TextStyle(fontSize:20.0)),
+        onPressed: moveToLogin,
+      )
+    ];
+  }
   }
 }
 
